517a00f24c6d6b676be9e774c35775f0f7af77c9
Elisa DROUOT <elisa.drouot@etu.uca.fr>
2022-11-10 16:46:02 +0000

    Upload New File

---
+++ Jeu Chaines de Markov/general.h
@@ -0,0 +1,32 @@
+#ifndef GENERAL_H
+#define GENERAL_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <time.h>
+
+#define ERREUR 10
+#define ACCUEIL 20
+#define JEU 30
+#define FINJEU 40
+
+#define EAU 0
+#define FEU 1
+#define TERRE 2
+#define DEFENSE 3
+
+#define ligneTexture 2
+#define colonneTexture 10
+#define nombreFichier 18
+#define tailleMaxFichiers 50
+
+#define nbrImageSpriteFini 10 //toutes les animations finies sont des sprites avec 10 images
+
+#define coefReducEcran 1.0
+
+#endif
