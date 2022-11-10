c6dc5a45e7525446e4597c31628d875f9b61fb7b
Elisa DROUOT <elisa.drouot@etu.uca.fr>
2022-11-10 16:46:20 +0000

    Upload New File

---
+++ Jeu Chaines de Markov/affichageAttaque.h
@@ -0,0 +1,8 @@
+#ifndef __AFFICHAGEATTAQUE_H__
+#define __AFFICHAGEATTAQUE_H__
+
+#include "general.h"
+
+SDL_Texture * textureAttaque(SDL_Texture * [ligneTexture][colonneTexture], int , int [2], int );
+
+#endif
